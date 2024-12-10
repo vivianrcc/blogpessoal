c644d4c755b2f46bd90ec26dc226f643934bae7d
vivianrcc <vivianrosanacc@gmail.com>
2024-12-10 00:18:52 -0300

    feat : temacontroller

---
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -0,0 +1,75 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.blogpessoal.model.Tema;
+import com.generation.blogpessoal.repository.TemaRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/temas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TemaController {
+	
+	@Autowired
+	private TemaRepository temaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll() {
+        return ResponseEntity.ok(temaRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tema> getById(@RequestParam long id) {
+        return temaRepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/descricao/{descricao}")
+    public ResponseEntity<List<Tema>> getByDescricao(@RequestParam String descricao) {
+        return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
+    }
+
+    @PostMapping
+    public ResponseEntity<Tema> post(@RequestBody Tema tema) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(temaRepository.save(tema));
+    }
+
+    @PutMapping
+    public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema) {
+        return temaRepository.findById(tema.getId())
+        .map(resp -> ResponseEntity.status(HttpStatus.CREATED)
+        .body(temaRepository.save(tema)))
+        .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        Optional<Tema> tema = temaRepository.findById(id);
+
+        if(tema.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        temaRepository.deleteById(id);
+    }    
+}
